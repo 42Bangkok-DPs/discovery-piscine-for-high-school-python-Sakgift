bafa7d603129b6e85ad70005bd06b540dee7ca72
Sakgift <manisakgife@gmail.com>
2024-10-08 13:31:25 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+print ("Enter a number less than 25")
+num = int(input())
+if num >25:
+    print ("Error")
+count = 0
+while (num < 25):
+    num = num + 1
+    print("Inside the loop,my variable is", num)
