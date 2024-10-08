dab979379394d308f7e1651c9a2a0606255ef36c
Sakgift <manisakgife@gmail.com>
2024-10-08 16:02:54 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,12 @@
+a = 0
+while a <= 10:
+        b = 0
+        print (f"Table de {a}:", end=' ')
+        while b <= 10:
+            print(a * b, end=' ')
+            b += 1
+        print() 
+        a += 1
+
+
+
