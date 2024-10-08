774281b225200bacca3dec585941a6765b8aae85
Sakgift <manisakgife@gmail.com>
2024-10-08 11:57:01 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,16 @@
+print ("Enter the first number:")
+first_number = float(input())
+print ("Enter the second number:")
+second_number = float(input())
+print (first_number,"X",second_number,"=",int(first_number*second_number))
+num = int(first_number*second_number)
+if num >0:
+    print("This number is positive")
+elif num <0:
+    print("This number is negetive")
+elif num ==0:
+    print("this number is both positive and negative")
+
+
+
+
